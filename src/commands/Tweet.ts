6ca24b2432d06491844048bcaefbcdcf3176a1b7
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-11-22 20:44:06 +0530

    Added all commands

--- src/commands/Tweet.ts
+++ src/commands/Tweet.ts
@@ -28,8 +28,8 @@ tweet
 tweet
 	.command('search')
 	.description('Fetch the list of tweets that match the given filter options')
-	.argument('[count]', 'The number of items to fetch')
-	.argument('[cursor]', 'The cursor to the batch of items to fetch')
+	.argument('[count]', 'The number of tweets to fetch')
+	.argument('[cursor]', 'The cursor to the batch of tweets to fetch')
 	.option('-f, --from <string>', "Matches the tweets made by list of given users, separated by ';'")
 	.option('-t, --to <string>', "Matches the tweets made to the list of given users, separated by ';'")
 	.option('-w, --words <string>', "Matches the tweets containing the given list of words, separated by ';'")
@@ -43,6 +43,72 @@ tweet
 			cursor,
 		);
 		output(tweets);
+	});
+
+// List
+tweet
+	.command('list')
+	.description('Fetch the list of tweets in the tweet list with the given id')
+	.argument('<id>', 'The id of the tweet list')
+	.argument('[count]', 'The number of tweets to fetch')
+	.argument('[cursor]', 'The cursor to the batch of tweets to fetch')
+	.action(async (id: string, count?: string, cursor?: string) => {
+		const tweets = await rettiwt.tweet.list(id, count ? parseInt(count) : undefined, cursor);
+		output(tweets);
+	});
+
+// Likes
+tweet
+	.command('likes')
+	.description('Fetch the list of users who liked the given tweets')
+	.argument('<id>', 'The id of the tweet')
+	.argument('[count]', 'The number of likers to fetch')
+	.argument('[cursor]', 'The cursor to the batch of likers to fetch')
+	.action(async (id: string, count?: string, cursor?: string) => {
+		const tweets = await rettiwt.tweet.favoriters(id, count ? parseInt(count) : undefined, cursor);
+		output(tweets);
+	});
+
+// Retweets
+tweet
+	.command('retweets')
+	.description('Fetch the list of users who retweeted the given tweets')
+	.argument('<id>', 'The id of the tweet')
+	.argument('[count]', 'The number of retweeters to fetch')
+	.argument('[cursor]', 'The cursor to the batch of retweeters to fetch')
+	.action(async (id: string, count?: string, cursor?: string) => {
+		const tweets = await rettiwt.tweet.retweeters(id, count ? parseInt(count) : undefined, cursor);
+		output(tweets);
+	});
+
+// Post
+tweet
+	.command('post')
+	.description('Post a tweet')
+	.argument('<text>', 'The text to post as a tweet')
+	.action(async (text: string) => {
+		const result = await rettiwt.tweet.tweet(text);
+		output(result);
+	});
+
+// Like
+tweet
+	.command('like')
+	.description('Like a tweet')
+	.argument('<id>', 'The tweet to like')
+	.action(async (id: string) => {
+		const result = await rettiwt.tweet.favorite(id);
+		output(result);
+	});
+
+// Retweet
+tweet
+	.command('retweet')
+	.description('Retweet a tweet')
+	.argument('<id>', 'The tweet to retweet')
+	.action(async (id: string) => {
+		const result = await rettiwt.tweet.retweet(id);
+		output(result);
 	});
 
 export default tweet;
--- src/commands/User.ts
+++ src/commands/User.ts
@@ -20,4 +20,59 @@ user.command('details')
 		output(details);
 	});
 
+// Following
+user.command('following')
+	.description('Fetch the list of users who are followed by the given user')
+	.argument('<id>', 'The id of the user')
+	.argument('[count]', 'The number of following to fetch')
+	.argument('[cursor]', 'The cursor to the batch of following to fetch')
+	.action(async (id: string, count?: string, cursor?: string) => {
+		const users = await rettiwt.user.following(id, count ? parseInt(count) : undefined, cursor);
+		output(users);
+	});
+
+// Followers
+user.command('followers')
+	.description('Fetch the list of users who follow the given user')
+	.argument('<id>', 'The id of the user')
+	.argument('[count]', 'The number of followers to fetch')
+	.argument('[cursor]', 'The cursor to the batch of followers to fetch')
+	.action(async (id: string, count?: string, cursor?: string) => {
+		const users = await rettiwt.user.followers(id, count ? parseInt(count) : undefined, cursor);
+		output(users);
+	});
+
+// Likes
+user.command('likes')
+	.description('Fetch the list of tweets liked by the given user')
+	.argument('<id>', 'The id of the user')
+	.argument('[count]', 'The number of liked tweets to fetch')
+	.argument('[cursor]', 'The cursor to the batch of liked tweets to fetch')
+	.action(async (id: string, count?: string, cursor?: string) => {
+		const users = await rettiwt.user.likes(id, count ? parseInt(count) : undefined, cursor);
+		output(users);
+	});
+
+// Timeline
+user.command('timeline')
+	.description('Fetch the tweets timeline the given user')
+	.argument('<id>', 'The id of the user')
+	.argument('[count]', 'The number of tweets to fetch')
+	.argument('[cursor]', 'The cursor to the batch of tweets to fetch')
+	.action(async (id: string, count?: string, cursor?: string) => {
+		const users = await rettiwt.user.timeline(id, count ? parseInt(count) : undefined, cursor);
+		output(users);
+	});
+
+// Replies
+user.command('replies')
+	.description('Fetch the replies timeline the given user')
+	.argument('<id>', 'The id of the user')
+	.argument('[count]', 'The number of replies to fetch')
+	.argument('[cursor]', 'The cursor to the batch of replies to fetch')
+	.action(async (id: string, count?: string, cursor?: string) => {
+		const users = await rettiwt.user.replies(id, count ? parseInt(count) : undefined, cursor);
+		output(users);
+	});
+
 export default user;
